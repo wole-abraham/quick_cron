db71a262530246a9b1fc05158c3928a45c33b767
wole <wole1702@gmail.com>
2025-11-26 01:35:33 +0100

    fp

--- cron.py
+++ cron.py
@@ -12,7 +12,6 @@ supabase = create_client(url, key)
 
 def fetch_today():
     today_str = date.today().isoformat()
-
     res = (
         supabase
         .table("survey")
@@ -25,8 +24,69 @@ def fetch_today():
     )
 
     return res.data or []
+def fetch_today2():
+    today_str = date.today().isoformat()
+
+    res = (
+        supabase
+        .table("hi-tech-activity")
+        .select(
+            "date_of_activity, reporter_name, side, "
+            "activity_category, activity_type, project_name, start_chainage, end_chainage"
+        )
+        .eq("date_of_activity", today_str)
+        .execute()
+    )
+
+    return res.data or []
 
 
+def activity_html(rows):
+    if not rows:
+        return "<h2>No survey records for today.</h2>"
+
+    html = """
+    <html>
+    <body>
+    <p>Dear All, Kindly check the activities for the hi-tech activity reports as summarized in the table below</p>
+    <table border="1" cellpadding="6" cellspacing="0" style="border-collapse: collapse;">
+        <tr style="background-color:#333;color:white;">
+            <th>date of activity</th>
+            <th>reporter name</th>
+            <th>project name</th>
+            <th>side</th>
+            <th>activity_category</th>
+            <th>activity_type</th>
+            <th>start chainage</th>
+            <th>end chainage</th>
+        </tr>
+    """
+
+    for row in rows:
+        html += "<tr>"
+        html += f"<td>{row['date_of_activity']}</td>"
+        html += f"<td>{row['reporter_name']}</td>"
+        html += f"<td>{row['project_name']}</td>"
+        html += f"<td>{row['side']}</td>"
+        html += f"<td>{row['activity_category']}</td>"
+        html += f"<td>{row['activity_type']}</td>"
+        html += f"<td>{row['start_chainage']}</td>"
+        html += f"<td>{row['end_chainage']}</td>"
+        html += "</tr>"
+
+    html += """
+    </table>
+    <p>In addition you will see the link below for the dashbaord showing details for each activity</p>
+    <p>
+  <a href="https://app.powerbi.com/view?r=eyJrIjoiNTNkYjUxZDUtNjAxZi00OTYyLTkxMDAtOWMwNmJhZTA1NmFhIiwidCI6IjlkZjZjN2Q4LTk4YzktNDliYS05NmZkLTBhNzIzMjVhZGMwYSJ9">
+    View Dashboard
+  </a>
+</p>
+    </body>
+    </html>
+    """
+
+    return html
 def bridge_html(rows):
     if not rows:
         return "<h2>No survey records for today.</h2>"
@@ -34,7 +94,7 @@ def bridge_html(rows):
     html = """
     <html>
     <body>
-    <p>Dear All, Kindly check the activities for the bridges daily construction summarized as the tabel below</p>
+    <p>Dear All, Kindly check the activities for the bridges daily construction summarized as the table below</p>
     <table border="1" cellpadding="6" cellspacing="0" style="border-collapse: collapse;">
         <tr style="background-color:#333;color:white;">
             <th>date_of_activity</th>
@@ -86,14 +146,26 @@ def send_webhook(html):
         data=html,
         headers={"Content-Type": "text/html"}
     )
+def send_webhook2(html):
+    data = {
+             'check': "activity",
+                "html": html
+        }
+    return requests.post(
+        
+        "https://hook.eu2.make.com/s4e53mqnndechr4ksi9pb3eyg8l2cum5",
+        json=data,
+        headers={"Content-Type": "application/json"}
+    )
 
 
 def main():
     rows = fetch_today()
-    
-    html = bridge_html(rows)
-    r = send_webhook(html)
-    print("Webhook:", r.status_code, r.text)
+    rows2 = fetch_today2()
+    html1 = bridge_html(rows)
+    html2 = activity_html(rows2)
+    one = send_webhook(html1)
+    two = send_webhook2(html2)
 
 
 if __name__ == "__main__":
